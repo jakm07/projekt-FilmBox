f37c9cb07572ff154d46e7d6af466c2957ce2f0e
Michaela Jakubikova <jakubi.mich@gmail.com>
2024-04-09 21:04:34 +0200

    ukol 3 - hamburger menu navigace

---
+++ spolecne.js
@@ -0,0 +1,9 @@
+const hamburger  = document.getElementById("menu-tlacitko")
+hamburger.addEventListener("click", function() {
+    let menu_polozky = document.getElementById("menu-polozky")
+    menu_polozky.classList.toggle("show")
+})
+
+
+
+
